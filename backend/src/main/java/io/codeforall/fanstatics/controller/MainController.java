b202d99befcd2456635b35ee62c16930e7628f5f
Leonardo Hugens <leonardohugens@protonmail.com>
2024-12-17 16:44:22 +0000

    Controller

--- backend/src/main/java/io/codeforall/fanstatics/controller/MainController.java
+++ backend/src/main/java/io/codeforall/fanstatics/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/queue")
+@RequestMapping("/api/user")
 public class MainController {
 
     private QueueService queueService;
