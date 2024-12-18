60b007fcc14e283fc69bfbf87edd083c71b01ee4
Leonardo Hugens <leonardohugens@protonmail.com>
2024-12-18 16:34:21 +0000

    backend is capable of creating user table in database

--- backend/src/main/java/io/codeforall/fanstatics/opticpal/persistence/dao/jpa/JpaUserDao.java
+++ backend/src/main/java/io/codeforall/fanstatics/opticpal/persistence/dao/jpa/JpaUserDao.java
@@ -1,7 +1,9 @@
 package io.codeforall.fanstatics.opticpal.persistence.dao.jpa;
 
 import io.codeforall.fanstatics.opticpal.persistence.model.User;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public class JpaUserDao extends GenericJpaDao<User>{
 
     public JpaUserDao(){
