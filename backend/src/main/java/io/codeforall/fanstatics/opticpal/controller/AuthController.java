049e716eb3a45f78ed73814d082f3cfe0ebec817
Leonardo Hugens <leonardohugens@protonmail.com>
2024-12-18 18:24:45 +0000

    backend user login working

--- backend/src/main/java/io/codeforall/fanstatics/opticpal/controller/AuthController.java
+++ backend/src/main/java/io/codeforall/fanstatics/opticpal/controller/AuthController.java
@@ -1,14 +1,13 @@
 package io.codeforall.fanstatics.opticpal.controller;
 
+import io.codeforall.fanstatics.opticpal.persistence.model.LoginRequest;
 import io.codeforall.fanstatics.opticpal.persistence.model.User;
 import io.codeforall.fanstatics.opticpal.services.AuthService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 
@@ -24,8 +23,8 @@ public class AuthController {
     }
 
     @GetMapping
-    public ResponseEntity<String> signUp(@Valid @RequestBody User user, BindingResult bindingResult){
-        if(bindingResult.hasErrors()){
+    public ResponseEntity<String> signUp(@Valid @RequestBody User user, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
             return ResponseEntity.badRequest().body("Invalid");
         }
         if (authService.isUserAlreadySignedUp(user.getEmail())) {
@@ -34,10 +33,20 @@ public class AuthController {
         try {
             authService.signup(user);
             return ResponseEntity.ok("User signed up successfully!");
-        } catch (Exception e){
+        } catch (Exception e) {
             return ResponseEntity.badRequest().body("User is already in the database");
         }
     }
 
+    @PostMapping("/login")
+    public ResponseEntity<Object> logIn(@Valid @RequestBody LoginRequest loginRequest) {
+
+        User user = authService.getUser(loginRequest.getEmail());
+        if(user.getPassword().equals(loginRequest.getPassword())){
+            return ResponseEntity.ok(user);
+        }
+
+        return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid email or password");
+    }
 
 }
--- backend/src/main/java/io/codeforall/fanstatics/opticpal/persistence/dao/jpa/JpaUserDao.java
+++ backend/src/main/java/io/codeforall/fanstatics/opticpal/persistence/dao/jpa/JpaUserDao.java
@@ -13,11 +13,26 @@ public class JpaUserDao extends GenericJpaDao<User>{
     public boolean existsByEmail(String email) {
         try {
             Long count = em.createQuery(
-                            "SELECT COUNT(u) FROM opticpal_users u WHERE u.email = :email", Long.class)
+                            "SELECT COUNT(u) FROM User AS u WHERE u.email = :email", Long.class)
                     .setParameter("email", email)
                     .getSingleResult();
             return count > 0;
         } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    public boolean existsByEmailAndPassword(String email, String password){
+        try {
+            Long count = em.createQuery(
+                    "SELECT COUNT(u) FROM User AS u" +
+                            " WHERE u.email = :email AND u.password = :password", Long.class)
+                    .setParameter("email", email)
+                    .setParameter("password", password)
+                    .getSingleResult();
+            return count > 0;
+        } catch (Exception e){
             e.printStackTrace();
             return false;
         }
