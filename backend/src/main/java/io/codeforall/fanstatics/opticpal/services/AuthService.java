65c8da19a734b17f0cbe5d169a699ebc54adff9f
Leonardo Hugens <leonardohugens@protonmail.com>
2024-12-18 16:42:31 +0000

    backend signup successfull

--- backend/src/main/java/io/codeforall/fanstatics/opticpal/services/AuthService.java
+++ backend/src/main/java/io/codeforall/fanstatics/opticpal/services/AuthService.java
@@ -4,6 +4,7 @@ import io.codeforall.fanstatics.opticpal.persistence.dao.jpa.JpaUserDao;
 import io.codeforall.fanstatics.opticpal.persistence.model.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AuthService {
@@ -15,6 +16,7 @@ public class AuthService {
         this.jpaUserDao = jpaUserDao;
     }
 
+    @Transactional
     public User signup(User user){
 
         return jpaUserDao.saveOrUpdate(user);
